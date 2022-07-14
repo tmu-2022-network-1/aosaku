c599aad604813f8b05f6f07eb326c2aaabc7d604
aosaku <84061699+aosaku@users.noreply.github.com>
2022-07-14 15:59:52 +0900

    0714

--- docs/main.js
+++ docs/main.js
@@ -1,9 +1,18 @@
-(function() {
-	"use strict";
-	const ua = navigator.userAgent.toLowerCase();
-	const isIos = ua.includes('iphone') || ua.includes('ipad');
-	const isAndroid = ua.includes('android') && ua.includes('chrome');
-	if (isIos || isAndroid) {
-		document.getElementById('scpr').classList.add('unsupported');
-	}
-})();
+const pageMostBottom = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener('scroll', function(){
+		let p=this.document.getElementsByClassName("img")[0];
+		const scr=Math.trunc(window.scrollY%500/100);
+		console.log(scr);
+		if(scr==0){
+			p.style.opacity=0.1;
+		}else p.style.opacity=1;
+
+		const scrollTop =document.documentElement.scrollTop;
+		if (scrollTop >= pageMostBottom) {
+			this.window.scrollTo(0,0);
+		}
+	});
+
+
+	
