ab1d96cfbaf8b03808aebd5617dbc4f46815ecef
aosaku <84061699+aosaku@users.noreply.github.com>
2022-07-15 15:04:51 +0900

    1

--- docs/main.js
+++ docs/main.js
@@ -3,6 +3,7 @@ let scr=0;
 let scrb=0;
 
 window.addEventListener('scroll', function(){
+	console.log(this.window.scrollY);
 	//連番雛形
 	let walk=new Array();
 	for(let i=0;i<17;i++){
@@ -10,7 +11,7 @@ window.addEventListener('scroll', function(){
 		if(walk[i]==scr){
 			this.document.getElementById(`a${i+1}`).style.opacity=1;
 		}else this.document.getElementById(`a${i+1}`).style.opacity=0;
-	}//
+	}
 
 	let stair=new Array();
 	for(let i=0;i<8;i++){
@@ -18,13 +19,10 @@ window.addEventListener('scroll', function(){
 		if(stair[i]==scrb){
 			this.document.getElementById(`b${i+1}`).style.opacity=1;
 		}else this.document.getElementById(`b${i+1}`).style.opacity=0;
-	}//
+	}
 
-		scr=Math.trunc(window.scrollY%1700/100);
-		scrb=Math.trunc(window.scrollY%800/100);
-		//表示されるもの
-
-
+		scr=Math.trunc(window.scrollY*4*1.2%1600/100);
+		scrb=Math.trunc(window.scrollY*3.65*1.2%800/100);
 
 		const scrollTop =document.documentElement.scrollTop;
 		if (scrollTop >= pageMostBottom) {
